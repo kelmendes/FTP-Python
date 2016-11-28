32701720884e58e153d5f2d59142e012aab91d34
klebsonmendes <klerbersonmendes@gmail.com>
2016-11-28 20:12:20 -0300

    Delete ftp.py

--- ftp.py
+++
@@ -1,8 +0,0 @@
-from ftplib import FTP #Importando a biblioteca 
-
-def main(): # Funcao para realizar login
-    ftp = FTP('keumendes.net16.net') #Informando o Dominio(Site)
-    ftp.login(user='a7856848', passwd='klebson') #Realizando Login
-    print("Voce esta trabalho no diretorio: ", ftp.pwd())
-    ftp.quit()
-main()
