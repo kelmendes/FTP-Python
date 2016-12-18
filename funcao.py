cbcf95fda0c9a475bedad45e6481c9a0fb5449ae
klebsonmendes <klerbersonmendes@gmail.com>
2016-12-18 18:43:13 -0300

    Update funcao.py

--- funcao.py
+++ funcao.py
@@ -1,14 +1,14 @@
-import ftplib, os#Importando a biblioteca
+import ftplib, os, glob#Importando a biblioteca
 
 ftp = None
 def conexao(site, usuario, senha):# Funcao para realizar login
-    global ftp #Tranformando a variavel ftp lobal
-    ftp = ftplib.FTP(site) #tentando acessar o site
+    global ftp
+    ftp = ftplib.FTP(site)
     print("Welcome:", ftp.getwelcome())
-    ftp.login(user= usuario, passwd= senha) #realizando loin no site
+    ftp.login(user= usuario, passwd= senha)
     print("Voce esta trabalho no diretorio: ", ftp.pwd())
 
-def menu_principal(): #menu principal
+def menu_principal():
 	print ()
 	print ('++----------------------------------------------------------------------------------++')
 	print ('++                              Menu Principal                                      ++')
@@ -16,7 +16,7 @@ def menu_principal(): #menu principal
 	print ('++  COD.        FUNCAO.                                                             ++')
 	print ('++  01          Listar Arquivo e DIretorios                                         ++')
 	print ('++  02          Acessar Um Diretorio do Servidor                                    ++')
-	print ('++  03          Enviar Arquivo                                                      ++')
+	print ('++  03          Enviar Arquivos                                                     ++')
 	print ('++  05          Criar Diretorio                                                     ++')
 	print ('++  06          Remover Diretorio                                                   ++')
 	print ('++  04          Sair                                                                ++')
@@ -30,20 +30,22 @@ def listar(argumento): #Listar arquivos e diretorios do servidor
 	print ('++----------------------------------------------------------------------------------++')
 	ftp.retrlines(argumento)
 
-def encerrando_conexão(): #encerra conexao
+def encerrando_conexão():
 	ftp.quit()
 
-def diretorio_cwd(caminho): #acessar diretorio === comando cwd no linux
+def diretorio_cwd(caminho):
 	ftp.cwd(caminho)
 
-def diretorio_mkd(caminho): #criando diretoriso == mkdir no linux
+def diretorio_mkd(caminho):
 	ftp.mkd(caminho)
 
-def diretorio_rmd(caminho): #apango dietorio == rmdir no linux
+def diretorio_rmd(caminho):
 	ftp.rmd(caminho)
 
-def uploader(caminho): #@Uploader de um arquivo
-    myfile = open(caminho,'rb')
-    filename = os.path.basename(caminho)
-    ftp.storlines('STOR ' + filename, myfile)
-    myfile.close()
+def uploader(caminho):
+    caminho += '*'
+    for dire in glob.glob(caminho):
+    	myfile = open(dire,'rb')
+    	filename = os.path.basename(dire)
+    	ftp.storlines('STOR ' + filename, myfile)
+    	myfile.close()
